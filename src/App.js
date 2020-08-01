d7195c3108a05d207e1e601b20c37cfd6d5798c6
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-08-01 12:29:19 +0200

    import of react-router elements, modification of app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-// import './App.css';
+import "./App.css";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+import { Route, Switch, Link } from "react-router-dom";
 
 function App() {
-  return <>Hello from app</>;
+  return (
+    <>
+      <Route path="/" component={Home} />
+      <Route path="/rooms" component={Rooms} />
+      <Route path="/single-room" component={SingleRoom} />
+    </>
+  );
 }
 
 export default App;
