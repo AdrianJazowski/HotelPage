3e14c39e43f7b09f1315b2b47c060b00c85b5b3f
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-08-04 20:12:34 +0200

    create context.js with provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 // import { RoomProvider } from "./context";
+import { RoomProvider } from "./context";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <RoomProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </RoomProvider>,
   document.getElementById("root")
 );
 
