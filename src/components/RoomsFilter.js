57bf370aaf8a9044e30e273761570a0ffdff9dea
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-08-23 13:19:12 +0200

    added more filter to roomsfilter.js, price,size and extras filters

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -69,6 +69,65 @@ export default function RoomsFilter({ rooms }) {
             {people}
           </select>
         </div>
+        {/* room price  */}
+        <div className="form-group">
+          <label htmlFor="price">room price ${price}</label>
+          <input
+            type="range"
+            name="price"
+            min={minPrice}
+            max={maxPrice}
+            id="price"
+            value={price}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+        {/* size */}
+        <div className="frorm-group">
+          <label htmlFor="size">room size</label>
+          <div className="size-inputs">
+            <input
+              type="number"
+              name="minSize"
+              id="size"
+              value={minSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+            <input
+              type="number"
+              name="maxSize"
+              id="size"
+              value={maxSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+          </div>
+        </div>
+        {/* extras */}
+        <div className="frorm-group">
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="breakfast"
+              id="breakfast"
+              checked={breakfast}
+              onChange={handleChange}
+            />
+            <label htmlFor="breakfast">Breakfast</label>
+          </div>
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="pets"
+              id="pets"
+              checked={pets}
+              onChange={handleChange}
+            />
+            <label htmlFor="pets">Pets</label>
+          </div>
+        </div>
       </form>
     </section>
   );
